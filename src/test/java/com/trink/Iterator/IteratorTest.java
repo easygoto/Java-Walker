06d7d74067826365d0218f459b1e59cc39defa47
trink <treelink@126.com>
2019-04-14 19:46:12 +0800

    Test: 规范化

--- src/test/java/com/trink/Iterator/IteratorTest.java
+++ src/test/java/com/trink/Iterator/IteratorTest.java
@@ -2,7 +2,22 @@ package com.trink.Iterator;
 
 import org.junit.Test;
 
-public class LinkedListTest {
+public class IteratorTest {
+
+    @Test
+    public void testArrayList() {
+        Connection conn = new ArrayList();
+        for (int i = 0; i < 15; i++) {
+            conn.add(new Cat(i));
+        }
+        System.out.println(conn.size());
+
+        Iterator it = conn.iterator();
+        while (it.hasNext()) {
+            Cat cat = (Cat) it.next();
+            System.out.println(cat);
+        }
+    }
 
     @Test
     public void testLinkedList() {
