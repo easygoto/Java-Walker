f637dbfbd22957c3314db570f6d5ccbd5bca1438
trink <treelink@126.com>
2019-04-21 18:29:38 +0800

    Command

---
+++ src/main/java/com/trink/Command/Command.java
@@ -0,0 +1,8 @@
+package com.trink.Command;
+
+public abstract class Command {
+
+    abstract boolean execute();
+
+    abstract boolean undo();
+}
---
+++ src/main/java/com/trink/Command/InterferenceCommand.java
@@ -0,0 +1,16 @@
+package com.trink.Command;
+
+public class InterferenceCommand extends Command {
+
+    @Override
+    boolean execute() {
+        System.out.println("interference ...");
+        return false;
+    }
+
+    @Override
+    boolean undo() {
+        System.out.println("cancel interference ...");
+        return true;
+    }
+}
