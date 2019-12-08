8cfe3862c6d3cc5c8507b2836967cf7811e4589e
trink <treelink@126.com>
2019-12-08 17:24:31 +0800

    变动修改

--- src/test/java/make/StatisticsValidLine.java
+++ src/test/java/make/StatisticsValidLine.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-public class statisticsValidLine {
+public class StatisticsValidLine {
 
     @Test
     public void run() throws FileNotFoundException {
