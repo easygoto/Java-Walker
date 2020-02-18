e9281ee18285db486bb21ea2bd7a24652c9802b3
trink <treelink@126.com>
2020-02-18 16:15:35 +0800

    - `feat` 聊天——加入多线程支持

--- src/main/java/gui/chat/ChatServer.java
+++ src/main/java/gui/chat/ChatServer.java
@@ -14,10 +14,11 @@ public class ChatServer {
     public static final String CLIENT_EXIT_MSG = "CLIENT_EXIT";
 
     public static void main(String[] args) {
-        ServerSocket serverSocket = null;
-        Socket client = null;
-        DataInputStream dis = null;
+        new ChatServer().start();
+    }
 
+    public void start() {
+        ServerSocket serverSocket = null;
         try {
             serverSocket = new ServerSocket(8888);
         } catch (IOException e) {
@@ -25,42 +26,65 @@ public class ChatServer {
         }
 
         try {
-            if (serverSocket != null) {
-                while (true) {
-                    client = serverSocket.accept();
-                    dis = new DataInputStream(client.getInputStream());
-
-                    while (true) {
-                        String msg = dis.readUTF();
-                        if (msg.equals(CLIENT_EXIT_MSG)) {
-                            close(dis, client);
-                            break;
-                        }
-                        System.out.printf("%s: %s\n", client.getInetAddress().getHostAddress(), msg);
-                    }
-                }
+            if (serverSocket == null) {
+                return;
+            }
+            while (true) {
+                Socket socket = serverSocket.accept();
+                Client client = new Client(socket);
+                new Thread(client).start();
             }
         } catch (Exception ignored) {
-        } finally {
-            close(dis, client);
         }
     }
 
-    public static void close(DataInputStream dis, Socket socket) {
-        try {
-            if (dis != null) {
-                dis.close();
+    private static class Client implements Runnable {
+
+        private Socket socket;
+
+        private DataInputStream dis;
+
+        public Client(Socket socket) {
+            this.socket = socket;
+            try {
+                this.dis = new DataInputStream(socket.getInputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-        try {
-            if (socket != null) {
-                System.out.printf("%s: closed ...\n", socket.getInetAddress().getHostAddress());
-                socket.close();
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    String msg = dis.readUTF();
+                    if (msg.equals(CLIENT_EXIT_MSG)) {
+                        close();
+                        break;
+                    }
+                    System.out.printf("%s: %s\n", socket.getInetAddress().getHostAddress(), msg);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        }
+
+        public void close() {
+            try {
+                if (dis != null) {
+                    dis.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (socket != null) {
+                    System.out.printf("%s: closed ...\n", socket.getInetAddress().getHostAddress());
+                    socket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
